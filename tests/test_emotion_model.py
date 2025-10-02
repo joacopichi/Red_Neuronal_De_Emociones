a0b0fc3ac0e51d3371c4e6d52c3a7fdc3dd5a2a2
Joaquin Piccirillo <pichijoaco1@gmail.com>
2025-10-02 00:46:17 -0300

    Ajuste de los test

--- tests/test_emotion_model.py
+++ tests/test_emotion_model.py
@@ -3,7 +3,9 @@ from src.model.emotion_model import EmotionModel
 
 def test_model_predict_returns_numpy_array():
     model = EmotionModel(vocab_size=5000, num_classes=6, max_len=100)
-    dummy_input = np.zeros((2, model.max_len))
+    input_shape = model.model.input_shape
+    max_len = input_shape[1]
+    dummy_input = np.zeros((2, max_len))
     preds = model.model.predict(dummy_input)
     assert isinstance(preds, np.ndarray)
     assert preds.shape[0] == 2
